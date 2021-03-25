4d15f608c51fbf38d76fa4468e916dab6bf99ef2
Marc Rodriguez <marc.herman.rodriguez@gmail.com>
2021-03-25 00:41:12 -0700

    fixed typos, updated JS about me action

--- script.js
+++ script.js
@@ -1,12 +1,14 @@
 const aboutMeLink = document.getElementById('about-me-link');
 const aboutMe = document.getElementById('about-me');
 const marcRodriguez = document.getElementById('marc-rod');
+const welcomeSection = document.getElementById('welcome-section');
 
 function showAboutMe() {
 
     function unhideSection() {
         aboutMe.style.display = 'block';
         marcRodriguez.style.cursor = 'auto';
+        welcomeSection.style.height = '200px';
     }
 
     if (aboutMe.style.display === '') {
